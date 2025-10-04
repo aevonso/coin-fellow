e40d98686676760e81f8b576f077df5aecc3eacb
aevonso <oddonenso@gmail.com>
2025-10-04 21:06:40 +0700

    fix/request for update groups

--- app/Http/Requests/Groups/UpdateGroupRequest.php
+++ app/Http/Requests/Groups/UpdateGroupRequest.php
@@ -3,3 +3,17 @@
 namespace App\Http\Requests\Groups;
 
 use App\Http\Requests\BaseRequest;
+
+class UpdateGroupsRequest extends BaseRequest {
+public function authorize(): bool {
+    return true;
+}
+
+public function rules(): array {
+    return [
+        'name' => 'sometimes|string|max:255',
+        'currency' => 'sometimes|string|size:3',
+        'description' => 'nullable|string|max:500',
+    ];
+}
+}
